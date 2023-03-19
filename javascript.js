2701f9666ba6fe83390e90f3fa9ebe01e62a7ab0
sreeadithya <79783628+sreeadithya@users.noreply.github.com>
2023-03-20 01:06:01 +0530

    blur screen when shortcut pressed

--- javascript.js
+++ javascript.js
@@ -1,33 +1,23 @@
-// Get the textarea element
 const textarea = document.querySelector('textarea');
-
-// Check if there is a saved value in localStorage and set it as the textarea value
 const savedValue = localStorage.getItem('textareaValue');
 if (savedValue) {
   textarea.value = savedValue;
 }
-
-// Save the text to localStorage when the user types
 textarea.addEventListener('input', () => {
   localStorage.setItem('textareaValue', textarea.value);
 });
 
-//javascript written by openai lol, i'll have to learn how to use js in the future
-// okay cool i think i finally know how git works across computers
-//test something
-//
 
-var isBlur = false;
-
+let isBlur = false;
 document.addEventListener("keydown", e => {
-  if (
-        isBlur==false
-        && e.ctrlKey
-        && e.key == "b"
-
-  ) {
-    document.getElementById('textarea').classList.add("blur")
-    isBlur==true
+  if (e.ctrlKey && e.key == "b") {
+    if (!isBlur) {
+      document.getElementById('textarea').classList.add("blur")
+      isBlur==true
+    }
+    else {
+      document.getElementById('textarea').classList.remove("blur")
+    }
+    isBlur=!isBlur
   }
-
 })
