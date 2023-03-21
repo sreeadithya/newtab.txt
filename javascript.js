5714561f155ae99c3e10182e23256818fe4c43bf
sreeadithya <79783628+sreeadithya@users.noreply.github.com>
2023-03-22 01:38:06 +0530

    added sidebar + darkmode. blur and darkmode status stored in memory

--- javascript.js
+++ javascript.js
@@ -1,26 +1,49 @@
-const textarea = document.querySelector('textarea');
+const textareaquery = document.getElementById('textarea');
 const savedValue = localStorage.getItem('textareaValue');
+
+
 if (savedValue) {
-  textarea.value = savedValue;
+  textareaquery.value = savedValue;
 }
-textarea.addEventListener('input', () => {
+
+textareaquery.addEventListener('input', () => {
   localStorage.setItem('textareaValue', textarea.value);
 });
 
+const textarea = document.getElementById('textarea')
 
-let isBlur = false;
+
+
+let isBlurRefresh = !JSON.parse(localStorage.getItem('isBlurRefresh'))
+
+
+if (isBlurRefresh != true) {
+  var isBlur = true;
+  textarea.classList.add("blur")
+}
+else {
+  var isBlur = false;
+}
+
+
+
 document.addEventListener("keydown", e => {
   if (e.ctrlKey && e.key == "b") {
-    if (!isBlur) {
-      document.getElementById('textarea').classList.add("blur")
-      isBlur==true
-    }
-    else {
-      document.getElementById('textarea').classList.remove("blur")
-    }
-    isBlur=!isBlur
+
+      if (!isBlur) {
+        textarea.classList.add("blur")
+        let isBlur = true
+      }
+      else if (isBlur) {
+        textarea.classList.remove("blur")
+        let isBlur = false
+      }
+isBlur=!isBlur
   }
+  localStorage.setItem('isBlurRefresh', isBlur)
 })
+
+
 
 
 const sidebar = document.getElementById('sidebar');
@@ -35,18 +58,49 @@ hoverchecker.addEventListener('mouseout', function handleMouseOutEvent() {
   sidebar.style.visibility = 'hidden'
 });
 
+
 const body = document.getElementById('body');
 
 const darkmodebtn = document.getElementById('darkmodebtn');
 
-var isDarkMode = false;
+const sidebutton = document.querySelectorAll('.sidebutton');
 
-if (!isDarkMode) {
-    body.style.backgroundColor = "#121212";
-    textarea.style.color = "#FFFFFF"
-  })
+
+isDarkModeStorage = JSON.parse(localStorage.getItem('isDarkModeStorage'))
+if (isDarkModeStorage == true) {
+  var isDarkMode = true;
+  textarea.style.color = "#FFFFFF";
+  body.style.backgroundColor = "#121212";
+  sidebar.style.backgroundColor = '#121212'
+  sidebar.style.outline = '2px solid white'
+  sidebutton.forEach(sidebutton => {
+    sidebutton.style.color = "#FFFFFF";
+  });
 }
 else {
-
+  var isDarkMode = false;
 }
-darkmodebtn.addEventListener('click', function changeStyle() {
+
+
+darkmodebtn.addEventListener('click', function(){
+  if (!isDarkMode) {
+        textarea.style.color = "#FFFFFF";
+        body.style.backgroundColor = "#121212";
+        sidebar.style.backgroundColor = '#121212'
+        sidebar.style.outline = '2px solid white'
+        sidebutton.forEach(sidebutton => {
+          sidebutton.style.color = "#FFFFFF";
+        });
+  }
+  else {
+        textarea.style.color = "#121212";
+        body.style.backgroundColor = "#FFFFFF";
+        sidebar.style.backgroundColor = '#FFFFFF'
+        sidebar.style.outline = '2px solid black'
+        sidebutton.forEach(sidebutton => {
+          sidebutton.style.color = "#121212";
+        });
+  }
+  isDarkMode = !isDarkMode
+  localStorage.setItem("isDarkModeStorage", isDarkMode)
+})
